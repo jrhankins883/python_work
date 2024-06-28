95e0e403715ce6b69a7ede099b328ad489134264
John Hankins <jrhankins883@yahoo.com>
2024-06-28 16:37:41 -0400

    two new python files and an updated hello world file

---
+++ simple_message.py
@@ -0,0 +1,2 @@
+fight_club_quote = "We are the all-singing, all-dancing crap of the world."
+print(fight_club_quote)
---
+++ simple_messages.py
@@ -0,0 +1,5 @@
+jackie_brown_quote = "If you had the chance to get away with a half a million dollars, would you take it?"
+print(jackie_brown_quote)
+
+jackie_brown_quote = "You didn't answer my question, if you had the chance to get away with a half a million dollars, would you take it?"
+print(jackie_brown_quote)
